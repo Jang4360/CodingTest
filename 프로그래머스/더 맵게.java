7bb8778a791a2106f8838e82a95384e04e474179
장주윤 <jooyoon4360@gmail.com>
2026-06-21 23:39:46 +0900

    Add comment to hotter solution

--- 프로그래머스/더 맵게.java
+++ 프로그래머스/더 맵게.java
@@ -3,6 +3,7 @@ import java.util.*;
 class Solution {
     public int solution(int[] scoville, int K) {
         int answer = 0;
+        // 가장 낮은 스코빌 지수를 빠르게 꺼내기 위해 우선순위 큐를 사용한다.
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         for (int num : scoville) {
             pq.offer(num);
