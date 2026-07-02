d125d0b75bd982f395f910706ea0a724fb10e010
Jooyoon-Jang <116609684+Jang4360@users.noreply.github.com>
2026-07-02 22:44:23 +0900

    [level 2] Title: 주식가격, Time: 6.64 ms, Memory: 72.6 MB -BaekjoonHub

--- Java/프로그래머스/2/42584. 주식가격/주식가격.java
+++ Java/프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,20 +1,17 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
-        int n = prices.length;
-        int[] answer = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            int time = 0;
-
-            for (int j = i + 1; j < n; j++) {
-                time++;
-
-                if (prices[j] < prices[i]) {
-                    break;
-                }
+        int[] answer = new int[prices.length];
+        
+        for(int i = 0; i<prices.length; i++) {
+            int s = prices[i];
+            int cnt = 0;
+            for (int j = i+1; j <prices.length; j++) {
+                int f = prices[j];
+                cnt++;
+                if (s>f) break;
             }
-            answer[i] = time;
+            answer[i] = cnt;
         }
         return answer;
     }
