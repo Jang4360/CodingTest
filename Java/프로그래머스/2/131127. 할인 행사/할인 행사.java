9fcfc9edb03060477fd17b45b151fa7c45e88f65
Jooyoon-Jang <116609684+Jang4360@users.noreply.github.com>
2026-07-26 00:52:25 +0900

    [level 2] Title: 할인 행사, Time: 41.79 ms, Memory: 128 MB -BaekjoonHub

--- Java/프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ Java/프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -2,32 +2,29 @@ import java.util.*;
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
-        Map<String,Integer> map = new HashMap<>();
-        for (int i = 0; i<want.length; i++) {
-            map.put(want[i], number[i]);
+        
+        int cnt = 0;
+        int len = want.length;
+        for (int i = 0; i<len; i++) {
+            cnt += number[i];
         }
         
-        for (int i = 0; i<discount.length; i++) {
-            if (discount.length-i<10) break;
-            if (!map.keySet().contains(discount[i])) continue;
-            Map<String,Integer> newMap = new HashMap<>(map);
-            
-            for (int j = i; j<i+10; j++) {
-                if (newMap.keySet().contains(discount[j])) {
-                    newMap.put(discount[j], newMap.get(discount[j])-1);
-                }
+        for (int i = 0; i<=discount.length-cnt; i++) {
+            Map<String,Integer> store = new HashMap<>();
+            for (int j = i; j<i+cnt; j++) {
+                store.put(discount[j], store.getOrDefault(discount[j],0)+1);
             }
             
-            boolean ok = true;
-            
-            for (int num : newMap.values()) {
-                if (num != 0) {
-                    ok = false;
+            boolean isAnswer = true;
+            for (int w = 0; w<len; w++) {
+                if (!store.containsKey(want[w]) || store.get(want[w]) != number[w]) {
+                    isAnswer = false;
                     break;
                 }
             }
-            if (ok==true) answer++;
+            if (isAnswer) answer++;
         }
+        
         return answer;
     }
 }
